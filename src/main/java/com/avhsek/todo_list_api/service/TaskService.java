913d293bbb37c07593574a83b8a34fee02cf8016
Abhishek <abhisam7007871@gmail.com>
2024-09-22 00:21:29 +0530

    final Commit

--- src/main/java/com/avhsek/todo_list_api/service/TaskService.java
+++ src/main/java/com/avhsek/todo_list_api/service/TaskService.java
@@ -3,10 +3,12 @@ package com.avhsek.todo_list_api.service;
 import com.avhsek.todo_list_api.model.Task;
 import com.avhsek.todo_list_api.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class TaskService {
 
     @Autowired
